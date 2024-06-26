4fe8aa62716227b9a82829f474223968b4b9c885
Gethin Davies <grd.davies@gmail.com>
2024-06-26 15:50:19 +0100

    Revise Luhn

--- haskell/luhn/src/Luhn.hs
+++ haskell/luhn/src/Luhn.hs
@@ -1,12 +1,13 @@
 module Luhn (isValid) where
 
-import Data.Char (isDigit, isSpace)
+import Data.Char (digitToInt, isDigit, isSpace)
 
 isValid :: String -> Bool
-isValid str =
-  (length stripped > 1) && all isDigit stripped && sum doubled `mod` 10 == 0
+isValid str
+  | length stripped <= 1 = False
+  | not (all isDigit stripped) = False
+  | otherwise = sum doubled `mod` 10 == 0
  where
   stripped = filter (not . isSpace) str
-  doubled = zipWith (\i x -> if odd i then (doubleSub . readChar) x else readChar x) [0 ..] (reverse stripped)
-  doubleSub x = let d = x * 2 in if d > 9 then d - 9 else d
-  readChar x = read [x]
+  doubled = zipWith ($) (cycle [id, double]) $ map digitToInt $ reverse stripped
+  double x = let d = x * 2 in if d > 9 then d - 9 else d
