1a92d83d34288e281d24263684d624a0ba1ab76f
Gethin Davies <grd.davies@gmail.com>
2024-06-30 13:50:42 +0100

    Refactor Minesweeper

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -1,6 +1,6 @@
 module Minesweeper (annotate) where
 
-import Data.Char (chr, ord)
+import Data.Char (intToDigit)
 
 annotate :: [String] -> [String]
 annotate board = map annotateRow $ enumerate board
@@ -14,24 +14,10 @@ annotate board = map annotateRow $ enumerate board
    where
     annotateCell (x, cell) = if isMine cell then cell else countMines
      where
-      countMines = if count > 0 then digit else ' '
-      count = vertical + horizontal + diagonal
-      digit = chr $ zero + count
-      zero = ord '0'
-
-      vertical = foldl (\a b -> a + if isMine b then 1 else 0) 0 [above, below]
-       where
-        above = if y > 0 then (board !! (y - 1)) !! x else ' '
-        below = if y < (nRows - 1) then (board !! (y + 1)) !! x else ' '
-
-      horizontal = foldl (\a b -> a + if isMine b then 1 else 0) 0 [left, right]
-       where
-        left = if x > 0 then row !! (x - 1) else ' '
-        right = if x < (nCols - 1) then row !! (x + 1) else ' '
-
-      diagonal = foldl (\a b -> a + if isMine b then 1 else 0) 0 [topLeft, topRight, bottomLeft, bottomRight]
-       where
-        topLeft = if x > 0 && y > 0 then board !! (y - 1) !! (x - 1) else ' '
-        topRight = if x < (nCols - 1) && y > 0 then board !! (y - 1) !! (x + 1) else ' '
-        bottomLeft = if x > 0 && y < (nRows - 1) then board !! (y + 1) !! (x - 1) else ' '
-        bottomRight = if x < (nCols - 1) && y < (nRows - 1) then board !! (y + 1) !! (x + 1) else ' '
+      countMines = if count > 0 then intToDigit count else ' '
+      count = countMineChars $ [board !! y' !! x' | x' <- [xMin .. xMax], y' <- [yMin .. yMax]]
+      countMineChars = foldl (\a b -> a + if isMine b then 1 else 0) 0
+      xMin = max (x - 1) 0
+      xMax = min (x + 1) (nCols - 1)
+      yMin = max (y - 1) 0
+      yMax = min (y + 1) (nRows - 1)
