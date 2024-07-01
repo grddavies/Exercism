850ae1c38eea7bf4655f6bf3563ab4ef7c2e76a9
Gethin Davies <grd.davies@gmail.com>
2024-07-01 14:12:54 +0100

    Alternate (worse?) solution to Minesweeper

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -1,23 +1,56 @@
 module Minesweeper (annotate) where
 
+import Control.Applicative (Applicative (liftA2))
 import Data.Char (intToDigit)
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
+
+data Cell a = Mine | Blank a
+  deriving (Show, Eq)
+
+instance Functor Cell where
+  fmap _ Mine = Mine
+  fmap f (Blank n) = Blank (f n)
+
+instance Applicative Cell where
+  pure = Blank
+  Mine <*> _ = Mine
+  _ <*> Mine = Mine
+  Blank f <*> Blank n = Blank (f n)
+
+type CoordMap = M.Map (Int, Int) (Cell Int)
 
 annotate :: [String] -> [String]
-annotate board = map annotateRow $ enumerate board
- where
-  isMine c = c == '*'
-  nRows = length board
-  nCols = if nRows == 0 then 0 else length $ head board
-  enumerate = zip [0 ..]
-  annotateRow :: (Int, String) -> String
-  annotateRow (y, row) = map annotateCell $ enumerate row
-   where
-    annotateCell (x, cell) = if isMine cell then cell else countMines
-     where
-      countMines = if count > 0 then intToDigit count else ' '
-      count = countMineChars $ [board !! y' !! x' | x' <- [xMin .. xMax], y' <- [yMin .. yMax]]
-      countMineChars = foldl (\a b -> a + if isMine b then 1 else 0) 0
-      xMin = max (x - 1) 0
-      xMax = min (x + 1) (nCols - 1)
-      yMin = max (y - 1) 0
-      yMax = min (y + 1) (nRows - 1)
+annotate board = coordMapToString (buildMap board) board
+
+addCell :: ((Int, Int), Char) -> CoordMap -> CoordMap
+addCell ((i, j), cell) m = if cell == '*' then updateMap (i, j) m else m
+
+updateMap :: (Int, Int) -> CoordMap -> CoordMap
+updateMap (i, j) m = M.insert (i, j) Mine $ foldr (\(x, y) -> M.insertWith (liftA2 (+)) (x, y) (Blank 1)) m (adjacentCells (i, j))
+
+adjacentCells :: (Int, Int) -> [(Int, Int)]
+adjacentCells (i, j) = [(x, y) | x <- [(i - 1) .. (i + 1)], y <- [(j - 1) .. (j + 1)], (i, j) /= (x, y)]
+
+buildMap :: [String] -> CoordMap
+buildMap board = foldr addCell M.empty $ concatMap addRowIndices (zip [0 ..] board)
+  where
+    addRowIndices (i, row) = zipWith (curry (addColIndex i)) [0 ..] row
+    addColIndex i (j, v) = ((i, j), v)
+
+coordMapToString :: CoordMap -> [String] -> [String]
+coordMapToString coordMap board = map buildRow [0 .. numRows - 1]
+  where
+    numRows = length board
+    numCols = if numRows > 0 then length (head board) else 0
+
+    buildRow :: Int -> String
+    buildRow i = map (buildCell i) [0 .. numCols - 1]
+
+    buildCell :: Int -> Int -> Char
+    buildCell i j = toChar . fromMaybe (Blank 0) $ M.lookup (i, j) coordMap
+
+    toChar :: Cell Int -> Char
+    toChar Mine = '*'
+    toChar (Blank 0) = ' '
+    toChar (Blank n) = intToDigit n
